f06e882a47328e40239f42c3811f09fffedaf6b4
Aziz <azizbrahim536@gmail.com>
2026-07-05 14:47:15 +0100

    First commit backend

---
+++ budgetpilotai/src/main/java/com/aziz/budgetpilotai/BudgetpilotaiApplication.java
@@ -0,0 +1,13 @@
+package com.aziz.budgetpilotai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetpilotaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetpilotaiApplication.class, args);
+	}
+
+}
---
+++ budgetpilotai/src/test/java/com/aziz/budgetpilotai/BudgetpilotaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aziz.budgetpilotai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetpilotaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
